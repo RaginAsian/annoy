7bb9ae41aad5ec4c01532b31e9c84a51687895f6
raginasian <jaultz@gmail.com>
2017-07-25 14:29:09 -0400

    first commit

---
+++ oldCode/annoy/annoy.js
@@ -0,0 +1,18 @@
+var answer = prompt("are we there yet?");
+
+while(answer !== "yes") {
+	var answer = prompt("are we there yet?"); 
+}
+
+alert("yay, we made it!");
+
+
+
+
+
+
+// if(answer === "yes") {
+// 	alert("yay, We made it!")
+// } else {
+// 	var answer = prompt("are we there yet?")
+// }
